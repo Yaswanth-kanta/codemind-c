67b01542a1d53f1d9450854fd5531e173b2eb3bd
Yaswanth-kanta <100055098+Yaswanth-kanta@users.noreply.github.com>
2022-10-11 23:50:20 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j==1||i==n||j==i)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     }     return 0; }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j==1||i==n||j==i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+    return 0;
+}
